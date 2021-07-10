06d6cc9d812056d772e962fb3d4bd0474fb7566b
Srashti Jain <srashti0204@gmail.com>
2021-07-10 23:39:09 +0530

    Added toPascalCase & toCamelCase

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-console.log("Updating string");
-
 module.exports = {
-  sayHello: () => {
-    console.log("Say Hello!!");
+  toUpperCase: (text) => {
+    return text.toUpperCase();
+  },
+  toLowerCase: (text) => {
+    return text.toLowerCase();
+  },
+  toPascalCase: (text) => {
+    return text
+      .replace(new RegExp(/[^\w\s]/, "g"), "")
+      .replace(
+        new RegExp(/\s+(.)(\w+)/, "g"),
+        ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+      )
+      .replace(new RegExp(/\s/, "g"), "")
+      .replace(new RegExp(/\w/), (s) => s.toUpperCase());
+  },
+  toCamelCase: (text) => {
+    return text
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      .replace(/\s+/g, "");
   },
 };
--- tester/app.js
+++
@@ -1,3 +0,0 @@
-const {sayHello} = require('@techvraksh/change-casex')
-
-sayHello()
